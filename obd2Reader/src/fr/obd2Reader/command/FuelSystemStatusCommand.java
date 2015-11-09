140523f6d03492d0592a2de3b49f5dd55fa8fde7
pierotLeo <fpierot.leo@gmail.com>
2015-11-10 00:10:25 +0100

    commit 8

--- obd2Reader/src/fr/obd2Reader/command/FuelSystemStatusCommand.java
+++ obd2Reader/src/fr/obd2Reader/command/FuelSystemStatusCommand.java
@@ -17,7 +17,7 @@ public class FuelSystemStatusCommand extends ObdCommand{
 				case 4 : status[i] = " Open loop due to engine load OR fuel cut due to deceleration"; break;
 				case 8 : status[i] = "Open loop due to system failure"; break;
 				case 16 : status[i] = "Closed loop, using at least one oxygen sensor but there is a fault in the feedback system"; break;
-				default : status[i] = "Invalid answer";
+				default : status[i] = "Invalid answer"; break;
 			}
 		}
 	}
