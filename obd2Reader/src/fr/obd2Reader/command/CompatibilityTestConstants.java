97054f909685ba647ff4a0b6e0a19e24b44638eb
Léo Piérot <fpierot.leo@gmail.com>
2015-12-14 20:40:14 +0100

    Architecture refont

--- obd2Reader/src/fr/obd2Reader/command/CompatibilityTestConstants.java
+++
@@ -1,5 +0,0 @@
-package fr.obd2Reader.command;
-
-public interface CompatibilityTestConstants {
-	public static final int COMPATIBILITY_RESPONSE_LENGTH = 5;
-}
--- obd2Reader/src/fr/obd2Reader/command/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package fr.obd2Reader.command;
-
-import java.util.ArrayList;
-
-public interface GlobalConstants {
-
-	public static final ArrayList<PidCell> pidTable = new ArrayList<PidCell>();
-	
-}
